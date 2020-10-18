98f9207010f31f4114f918cc5de7a42d559f07b4
raitichan <rusuke@t-net.ne.jp>
2020-10-18 22:25:25 +0900

    add loging for App.xaml.cs

--- AdvancedVRChatPictureSorter/App.xaml.cs
+++ AdvancedVRChatPictureSorter/App.xaml.cs
@@ -1,25 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using AdvancedVRChatPictureSorter.Plugin;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 
 namespace AdvancedVRChatPictureSorter {
 	/// <summary>
 	/// App.xaml の相互作用ロジック
 	/// </summary>
 	public partial class App : Application {
+		/// <summary>
+		/// Logger
+		/// </summary>
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
+		/// <summary>
+		/// プラグインマネージャー
+		/// </summary>
+		private readonly PluginManager pluginManager = new PluginManager(); 
+
+		/// <summary>
+		/// タスクトレイアイコン
+		/// </summary>
 		private NotifycationWrapper notify = null;
 
+		/// <summary>
+		/// 初期化処理
+		/// </summary>
+		private void Initialize() {
+			logger.Info("Initialize application.");
+		}
+
+
 		private void StartEvent(object sender, StartupEventArgs e) {
+			logger.Info("Start application.");
+
 			this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
 			this.notify = new NotifycationWrapper();
+			this.Initialize();
 		}
 
 		private void ExitEvent(object sender, ExitEventArgs e) {
+			logger.Info("Finalze.");
 			this.notify.Dispose();
 		}
 
