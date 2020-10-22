787da9679d6a15981eaedbe43b89929dd1b3f14b
raiti-chan <rusuke@t-net.ne.jp>
2020-10-22 18:24:22 +0900

    add plugin manager

---
+++ PluginLibrary/Manager/PluginEntry.cs
@@ -0,0 +1,38 @@
+﻿using Raitichan.AdvancedVRChatPictureSorter.Library.Interface;
+using System.IO;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Library.Manager {
+	/// <summary>
+	/// プラグイン管理用
+	/// </summary>
+	internal class PluginEntry {
+
+		/// <summary>
+		/// プラグイン
+		/// </summary>
+		public IPlugin PluginInterface { get; private set; }
+
+		/// <summary>
+		/// プラグインDLLへのパス
+		/// </summary>
+		public FileInfo PluginPath { get; private set; }
+
+		/// <summary>
+		/// プラグインオブジェクトの初期化
+		/// </summary>
+		/// <param name="context">プラグインのエントリーポイント</param>
+		/// <param name="path">プラグインパス</param>
+		public PluginEntry(FileInfo path ,IPlugin context) {
+			this.PluginInterface = context;
+			this.PluginPath = path;
+		}
+
+		/// <summary>
+		/// プラグインを開放します。
+		/// </summary>
+		public void Dispose() {
+			this.PluginInterface.Dispose();
+		}
+
+	}
+}
---
+++ PluginLibrary/Manager/PluginManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using NLog;
+using Raitichan.AdvancedVRChatPictureSorter.Library.Interface;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Library.Manager {
+	/// <summary>
+	/// プラグインマネージャー
+	/// </summary>
+	internal class PluginManager {
+
+		/// <summary>
+		/// Logger
+		/// </summary>
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+		/// <summary>
+		/// 読み込まれたプラグイン
+		/// </summary>
+		private readonly Dictionary<string, PluginEntry> plugins = new Dictionary<string, PluginEntry>();
+
+
+		/// <summary>
+		/// 指定されたディレクトリからプラグインを読み込む
+		/// </summary>
+		/// <param name="rootDirectory">ルートディレクトリ</param>
+		public void LoadPlugins(DirectoryInfo rootDirectory) {
+			logger.Info("Load plugins.");
+			foreach (FileInfo dll in rootDirectory.GetFiles("*.dll")) {
+				logger.Info("Find Dll {0}", dll.FullName);
+				this.LoadPlugin(dll);
+			}
+		}
+
+		/// <summary>
+		/// 指定されたパスのプラグインを読み込む
+		/// </summary>
+		/// <param name="dllFile">読み込むDLL</param>
+		public void LoadPlugin(FileInfo dllFile) {
+			logger.Info("Load Plugin {0}", dllFile);
+			
+			IPlugin plugin = null;
+			Assembly asm = Assembly.LoadFrom(dllFile.FullName);
+
+			var types = from type in asm.GetTypes() where !(type.IsInterface || type.IsAbstract) select type;
+			foreach (Type type in types) {
+				logger.Debug("Find Type : {0}", type.FullName);
+				if (type.IsAssignableFrom(typeof(Object))) {
+					logger.Info("Find Plugin Entrypoint {0}", type.Name);
+					//plugin = Activator.CreateInstance(type) as IPluginInterface;
+					break;
+				}
+			}
+
+			if (plugin == null) {
+				logger.Warn("Not found PluginInterface.");
+				return;
+			}
+
+			PluginEntry pe = new PluginEntry(dllFile, plugin);
+			this.plugins.Add(plugin.PluginName, pe);
+		}
+
+		/// <summary>
+		/// プラグインマネージャを破棄します。
+		/// </summary>
+		public void Dispose() {
+			foreach(KeyValuePair<string, PluginEntry> plugin in this.plugins) {
+				plugin.Value.Dispose();
+			}
+
+			this.plugins.Clear();
+		}
+
+	}
+}
