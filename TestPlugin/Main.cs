07401ce91396bf5bbe2f47741f9b3101a686eca5
raiti-chan <rusuke@t-net.ne.jp>
2020-10-22 18:44:49 +0900

    add Main

---
+++ TestPlugin/Main.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using Raitichan.AdvancedVRChatPictureSorter.Library.Interface;
+using System;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Plugins.TestPlugin {
+
+	/// <summary>
+	/// テストプラグイン
+	/// </summary>
+	public class Main : IPlugin {
+
+		/// <summary>
+		/// Logger
+		/// </summary>
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+
+		string IPlugin.PluginName { get; } = "TestPlugin";
+
+		string IPlugin.PluginDesctiption { get; } = "Test!!";
+
+		Version IPlugin.PluginVersion { get; } = new Version(1, 0, 0);
+
+		void IPlugin.Dispose() {
+			logger.Info("Test Plugin Dispose");
+		}
+
+		void IPlugin.Initialize() {
+			logger.Info("Test Plugin Initialize");
+		}
+	}
+}
