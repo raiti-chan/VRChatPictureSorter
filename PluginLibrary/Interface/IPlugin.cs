f8b1ecae70c1e08f81e1f41da0b5d6597d11230f
raiti-chan <rusuke@t-net.ne.jp>
2020-10-22 17:51:49 +0900

    Add IPlugin

---
+++ PluginLibrary/Interface/IPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Library.Interface {
+	interface IPlugin {
+
+		/// <summary>
+		/// プラグイン名
+		/// </summary>
+		string PluginName { get; }
+
+		/// <summary>
+		/// プラグイン詳細
+		/// </summary>
+		string PluginDesctiption { get; }
+
+		/// <summary>
+		/// プラグインバージョン
+		/// </summary>
+		Version PluginVersion { get; }
+
+		/// <summary>
+		/// 初期化処理
+		/// </summary>
+		void Initialize();
+
+
+		/// <summary>
+		/// プラグインのアンロード処理
+		/// </summary>
+		void Dispose();
+
+	}
+}
