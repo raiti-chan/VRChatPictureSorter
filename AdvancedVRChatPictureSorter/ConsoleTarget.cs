53ef4ae2119271c21f379ce6267ebca4650d0c08
raiti-chan <rusuke@t-net.ne.jp>
2020-10-22 23:03:42 +0900

    ログウィンドウ用ログターゲットの追加

---
+++ AdvancedVRChatPictureSorter/ConsoleTarget.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using NLog.Common;
+using NLog.Targets;
+using System;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Core {
+	/// <summary>
+	/// コンソールウィンドウへ出力するターゲット
+	/// </summary>
+	[Target("ConsoleWrapper")]
+	internal class ConsoleTarget : TargetWithLayout {
+
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		/// <param name="name">ターゲット名</param>
+		public ConsoleTarget(string name) {
+			this.Name = name;
+		}
+
+
+		protected override void Write(LogEventInfo logEvent) {
+			// TODO: ログコンソールへ出力
+		}
+
+	}
+}
