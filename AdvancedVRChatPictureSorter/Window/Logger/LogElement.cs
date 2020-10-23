70185d6837127f692b2ef2b3e1059e65e7d28faa
raiti-chan <rusuke@t-net.ne.jp>
2020-10-23 23:22:13 +0900

    add logelement paramater

--- AdvancedVRChatPictureSorter/Window/Logger/LogElement.cs
+++ AdvancedVRChatPictureSorter/Window/Logger/LogElement.cs
@@ -1,11 +1,17 @@
 ﻿using NLog;
 using System;
+using System.Collections.Generic;
 
 namespace Raitichan.AdvancedVRChatPictureSorter.Core.Window.Logger {
 	/// <summary>
 	/// ログの最小単位オブジェクト
 	/// </summary>
 	internal class LogElement {
+
+		/// <summary>
+		/// ログID
+		/// </summary>
+		public int Id { get; private set; }
 
 		/// <summary>
 		/// ログレベル
@@ -18,18 +24,40 @@ namespace Raitichan.AdvancedVRChatPictureSorter.Core.Window.Logger {
 		public DateTime Time { get; private set; }
 
 		/// <summary>
+		/// ロガー名
+		/// </summary>
+		public string LoggerName { get; private set; }
+
+		/// <summary>
 		/// ログメッセージ
 		/// </summary>
 		public string Message { get; private set; }
+
+		/// <summary>
+		/// ログプロパティ、空の場合null
+		/// </summary>
+		public IDictionary<object, object> Properties { get; private set; } = null;
+
+		/// <summary>
+		/// パラメータ
+		/// </summary>
+		public object[] Parameters { get; private set; } = null;
 
 		/// <summary>
 		/// LogEventInfoからLogElementの生成
 		/// </summary>
 		/// <param name="info">LogEventInfo</param>
 		public LogElement (LogEventInfo info) {
+			this.Id = info.SequenceID;
 			this.Level = info.Level;
-			this.Message = info.Message;
 			this.Time = info.TimeStamp;
+			this.LoggerName = info.LoggerName;
+			this.Message = info.FormattedMessage;
+			this.Parameters = info.Parameters;
+
+			if (info.Properties.Count > 0) {
+				this.Properties = info.Properties;
+			}
 		}
 
 	}
