3823e5ec0bbda1707f3bdf8009651f7b40eb4ede
raiti-chan <rusuke@t-net.ne.jp>
2020-10-24 17:10:41 +0900

    logを取得するメソッドの追加

--- AdvancedVRChatPictureSorter/Window/Logger/LogStack.cs
+++ AdvancedVRChatPictureSorter/Window/Logger/LogStack.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,16 @@ namespace Raitichan.AdvancedVRChatPictureSorter.Core.Window.Logger {
 		/// <param name="info">ログinfo</param>
 		public void WriteLog(LogEventInfo info) {
 			this.logElements.Add(new LogElement(info));
+		}
+
+
+		/// <summary>
+		/// 読み取り専用でログリストを取得する。
+		/// </summary>
+		public ReadOnlyCollection<LogElement> LogElements {
+			get {
+				return this.logElements.AsReadOnly();
+			}
 		}
 
 	}
