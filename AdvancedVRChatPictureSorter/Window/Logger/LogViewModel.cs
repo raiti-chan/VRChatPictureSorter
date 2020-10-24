177a86c85080d4d071e73edad215841a4d03f849
raiti-chan <rusuke@t-net.ne.jp>
2020-10-24 17:10:56 +0900

    ウィンドウ関係のクラスの追加

---
+++ AdvancedVRChatPictureSorter/Window/Logger/LogViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Core.Window.Logger {
+	/// <summary>
+	/// ログウィンドウのビューモデル
+	/// </summary>
+	internal class LogViewModel : INotifyPropertyChanged {
+
+		/// <summary>
+		/// ログ情報
+		/// </summary>
+		public LogStack LogStack { get; set; }
+
+		/// <summary>
+		/// ログリスト
+		/// </summary>
+		public IList<LogElement> LogElements {
+			get {
+				if (LogStack == null) return null;
+				return this.LogStack.LogElements;
+			}
+		}
+
+
+		/// <summary>
+		/// プロパティ変更通知イベント
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// プロパティの変更の通知
+		/// </summary>
+		/// <param name="propertyName">プロパティ名</param>
+		private void RaisePropertyChanged([CallerMemberName]string propertyName = null) {
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
