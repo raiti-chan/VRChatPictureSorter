fb6cde61fc0fdb8465ac84008dc602f6eeeef9b0
raiti-chan <rusuke@t-net.ne.jp>
2020-10-25 22:43:47 +0900

    ログ削除コマンドの追加

---
+++ AdvancedVRChatPictureSorter/Window/Logger/Command/LogClearCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Core.Window.Logger.Command {
+	/// <summary>
+	/// ログを削除するコマンド
+	/// </summary>
+	internal class LogClearCommand : ICommand {
+
+		/// <summary>
+		/// 実行可能か変更された際に通知する。
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+
+		/// <summary>
+		/// 実行可能か否か。常に実行可能なのでtrue。
+		/// </summary>
+		/// <param name="parameter">パラメータ</param>
+		/// <returns>true</returns>
+		public bool CanExecute(object parameter) {
+			return true;
+		}
+
+		/// <summary>
+		/// 実行。ログを削除します。
+		/// </summary>
+		/// <param name="parameter">パラメータ</param>
+		public void Execute(object parameter) {
+			App.CurrentApp.LogStack.Clear();
+		}
+	}
+}
