2b17669d5835caeeb6a728b750ed67fb0e30b27f
raiti-chan <rusuke@t-net.ne.jp>
2020-10-31 18:20:42 +0900

    FatalExceptinDialogの追加

---
+++ AdvancedVRChatPictureSorter/Window/FatalExceptionDialog/FatalExceptionDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Core.Window.FatalExceptionDialog {
+	/// <summary>
+	/// FatalExceptionDialog.xaml の相互作用ロジック
+	/// </summary>
+	internal partial class FatalExceptionDialog : System.Windows.Window {
+		public FatalExceptionDialog(Exception e) {
+			InitializeComponent();
+			if (this.DataContext is ExceptionViewModel viewModel) {
+				viewModel.Exception = e;
+			}
+		}
+	}
+}
