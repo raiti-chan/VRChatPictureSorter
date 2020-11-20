3c6ee56fdda57baaaef3389ae7c2456b3e110dc0
raiti-chan <rusuke@t-net.ne.jp>
2020-11-20 12:09:34 +0900

    メニュー追加処理を実装

--- AdvancedVRChatPictureSorter/Module/CoreModule.cs
+++ AdvancedVRChatPictureSorter/Module/CoreModule.cs
@@ -1,9 +1,6 @@
-﻿using Raitichan.AdvancedVRChatPictureSorter.Library.Module;
+﻿using NLog;
+using Raitichan.AdvancedVRChatPictureSorter.Library.Module;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Raitichan.AdvancedVRChatPictureSorter.Core.Module {
 	/// <summary>
@@ -12,9 +9,28 @@ namespace Raitichan.AdvancedVRChatPictureSorter.Core.Module {
 	internal class CoreModule : ICoreModule {
 
 		/// <summary>
+		/// Logger
+		/// </summary>
+		private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+
+		/// <summary>
 		/// コアモジュールインスタンス
 		/// </summary>
 		public static CoreModule Instance { get; } = new CoreModule();
+
+		public void Initialize() {
+			logger.Info("Init CoreModule.");
+		}
+
+		/// <summary>
+		/// タスクトレイアイコンにメニューを追加します。
+		/// </summary>
+		/// <param name="name">項目名</param>
+		/// <param name="e">押下時イベントハンドラ</param>
+		public void AddNotifycationMenu(string name, EventHandler e) {
+			App.CurrentApp.Notifycation.AddMenuItem(name, e);
+		}
 
 	}
 }
