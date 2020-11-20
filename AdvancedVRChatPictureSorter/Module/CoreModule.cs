45716eb16f53517a898a8a51157ac87d2eee7c03
raiti-chan <rusuke@t-net.ne.jp>
2020-11-20 11:24:09 +0900

    コアモジュールの実装追加

---
+++ AdvancedVRChatPictureSorter/Module/CoreModule.cs
@@ -0,0 +1,20 @@
+﻿using Raitichan.AdvancedVRChatPictureSorter.Library.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raitichan.AdvancedVRChatPictureSorter.Core.Module {
+	/// <summary>
+	/// アプリケーションコアモジュール
+	/// </summary>
+	internal class CoreModule : ICoreModule {
+
+		/// <summary>
+		/// コアモジュールインスタンス
+		/// </summary>
+		public static CoreModule Instance { get; } = new CoreModule();
+
+	}
+}
