9988b7e30ec69578bd2c079716702401d8f96858
raiti-chan <rusuke@t-net.ne.jp>
2020-11-20 12:09:15 +0900

    タスクトレイメニュー追加メソッドの追加

--- PluginLibrary/Module/CoreModule.cs
+++ PluginLibrary/Module/CoreModule.cs
@@ -1,5 +1,15 @@
 ﻿
+using System;
+
 namespace Raitichan.AdvancedVRChatPictureSorter.Library.Module {
 	public interface ICoreModule {
+
+		/// <summary>
+		/// タスクトレイアイコンにメニューを追加します。
+		/// </summary>
+		/// <param name="name">項目名</param>
+		/// <param name="e">押下時イベントハンドラ</param>
+		void AddNotifycationMenu(string name, EventHandler e);
+
 	}
 }
